c0461cf4069ca8aefcd1d4fe3f887795849a2859
M. Brent Harp <brharp@uoguelph.ca>
2010-01-26 09:34:10 -0500

    Initial checkin of zimlet support files.

--- zimlet/gimlet.js
+++ zimlet/gimlet.js
@@ -2,14 +2,107 @@ function GimletHandlerObject() {}
 GimletHandlerObject.prototype = new ZmZimletBase();
 GimletHandlerObject.prototype.constructor = GimletHandlerObject;
 
+baseURI = "baseURI:";
+
+function resolveURI (relativeURI, baseURI) {
+  if (relativeURI.indexOf(":") < 0) {
+    uri = baseURI + relativeURI;
+  } else {
+    uri = relativeURI;
+  }
+  if (uri.indexOf("portlet:") == 0) {
+    return createPortletURL(uri);
+  } else {
+    return uri;
+  }
+}
+
+
+function createURL (method, mode, state, secure, params) {
+  return "javascript:"+method+"(\""+mode+"\",\""+state+"\",\""+secure+"\","
+             +params+")";
+}
+
+function PortletMode () {}
+PortletMode.VIEW = "view";
+PortletMode.EDIT = "edit";
+PortletMode.HELP = "help";
+
+function WindowState () {}
+WindowState.MAXIMIZED = "maximized";
+WindowState.MINIMIZED = "minimized";
+WindowState.NORMAL = "normal";
+
+// Creates a URI from a portlet: scheme URI.
+// In a Zimlet, portlet: scheme URIs are implemented
+// as javascript: URIs that call one of render() or 
+// or processAction.
+GimletHandlerObject
+function createPortletURL (uri) {
+  var method;
+  var mode = PortletMode.VIEW;
+  var state = WindowState.NORMAL;
+  var secure = true;
+  var parameters = [];
+  parts = uri.substr(8).split("?", 2);
+  if (parts.length > 0) {
+    var pathComp = parts[0];
+    var path = pathComp.split(";", 2);
+    method = path[0];
+    if (path.length > 1) {
+      var params = path[1].split(";");
+      for (var i = 0; i < params.length; i++) {
+        var nv = params[i].split("=");
+        if (nv.length == 2) {
+          var name = nv[0].toLowerCase();
+          var value = nv[1].toLowerCase();
+          if ("mode" == name)
+            { mode = value; }
+          else if ("state" == name)
+            { state = value; }
+          else if ("secure" == name)
+            { secure = value; }
+        }
+      }
+    } else { // there was nothing after portlet:
+      method = "render";
+    }
+  }
+  if (parts.length == 2) {
+    query = parts[1];
+    pairs = query.split("&");
+    for (var i = 0; i < pairs.length; i++){ 
+      var nv = pairs[i].split("=");
+      if (nv.length = 2) {
+        var name = nv[0];
+        var value = nv[1];
+        parameters.push("\""+name+"\":\""+value+"\"");
+      }
+    }
+  }
+  return createURL(method,mode,state,secure,"{"+parameters.join(",")+"}");
+}
+
 GimletHandlerObject.prototype.singleClicked =
-function(canvas) {
+function (canvas) {
   // Set up the dialog.
   var view = new DwtComposite(this.getShell());
   view.setSize(320, 480);
+  this.content = document.createElement("div");
+  this.content.style.width = 320;
+  this.content.style.height = 480;
+  this.content.style.overflow = 'scroll';
+  view.getHtmlElement().appendChild(this.content);
   var title = this.getMessage("gimletName");
   canvas = this._createDialog({view: view, title: title});
   canvas.view = view;
+  this.render("view", "normal", true, {});
+  canvas.popup(); 
+  return canvas;
+};
+
+GimletHandlerObject.prototype.render =
+function (mode,state,secure,params) {
   // Get content.
   var req = new XMLHttpRequest();
   req.open("GET", this.getResource("snoop.jsp"), false);
@@ -18,7 +111,7 @@ function(canvas) {
   req.setRequestHeader("X-Portlet-Context-Path", this.getResource(""));
   req.setRequestHeader("X-Portlet-Locale", navigator.language);
   req.setRequestHeader("X-Portlet-Namespace", "");
-  req.setRequestHeader("X-Portlet-Portlet-Mode", "view");
+  req.setRequestHeader("X-Portlet-Portlet-Mode", mode);
   req.setRequestHeader("X-Portlet-Parameter", "");
   var hv = "";
   var props = this.xmlObj().userProperties;
@@ -37,24 +130,32 @@ function(canvas) {
   req.setRequestHeader("X-Portlet-Server-Port", "");
   req.setRequestHeader("X-Portlet-Scheme", "");
   req.setRequestHeader("X-Portlet-User-Info", "");
-  req.setRequestHeader("X-Portlet-Window-State", "normal");
+  req.setRequestHeader("X-Portlet-Window-State", state);
   req.send("");
   if (req.readyState == 4 && req.status == 200) {
-    var div = document.createElement("div");
-    div.style.width = 320;
-    div.style.height = 480;
-    div.style.overflow = 'scroll';
-    view.getHtmlElement().appendChild(div);
+    var div = this.content;
     div.innerHTML = req.responseText;
+    // Process links.
+    var names=["a"];
+    var attrs=["href"];
+    for (var i = 0; i < names.length; i++) {
+      var name = names[i];
+      var elts = div.getElementsByTagName(name);
+      for (var j = 0; j < elts.length; j++) {
+        var e = elts[j];
+        for (var k = 0; k < attrs.length; k++) {
+          if (e.hasAttribute(attrs[k])) {
+            var a = e.getAttribute(attrs[k]);
+            e.setAttribute(attrs[k], resolveURI(a));
+          }
+        }
+      }
+    }
   }
-  canvas.popup(); 
-  // Display content.
-//  var view = new DwtComposite(this.getShell());
-//  view.setSize(Dwt.DEFAULT, Dwt.DEFAULT);
-//  var title = this.getMessage("gimletName");
-//  canvas = this._createDialog({view: view, title: title});
-//  canvas.view = view;
-//  canvas.popup(); 
-  return canvas;
-};
+}
 
+GimletHandlerObject.prototype.init =
+function () {
+  this.namespace = "G" + (Math.random() * 10);
+  GimletHandlerObject[this.namespace] = this;
+}
--- zimlet/render.jsp
+++ zimlet/render.jsp
@@ -1,5 +1,20 @@
-
+<%@ page import="ca.uoguelph.ccs.portal.rest.Header" %>
+<%@ page import="ca.uoguelph.ccs.ws.XMLHttpRequestObject" %>
 <% 
-Proxy p = new ZimletProxy();
-p.doProxy(request, response);
+
+XMLHttpRequestObject xhr = new XMLHttpRequestObject();
+Header[] requestHeaders = Header.renderRequestHeaders;
+for (int i = 0; i < requestHeaders.length; i++) {
+  String name = requestHeaders[i].getName();
+  xhr.setRequestHeader(name, request.getHeader(name));
+}
+xhr.open("GET", "http://localhost:7080/service/zimlet/_dev/gimlet/snoop.jsp", false);
+xhr.send(request.getReader());
+Header[] responseHeaders = Header.responseHeaders;
+for (int i = 0; i < responseHeaders.length; i++) {
+  String name = responseHeaders[i].getName();
+  response.setHeader(name, xhr.getResponseHeader(name));
+}
+
+
 %>
